b21796034878c20156ddc343b79cc6a0dab01cf3
GoldNicolas <nicolas.guerra0804@gmail.com>
2025-02-03 00:56:23 +0000

    First commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,12 @@
-
+public class BankAccount {
+    public static void main(String[] args) {
+        double balance = 2175.50;
+        balance - 302.50;
+        balance + 50.03;
+        balance/2 + 20;
+        balance-1;
+        balance*2;
+        balance+1;
+        System.out.println(balance);
+    }
+}
